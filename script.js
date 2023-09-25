71cb6c0de72eb28f51074316e351867b6ab4ae2b
Akash pandey <106162919+bitu99@users.noreply.github.com>
2023-09-25 09:11:26 +0000

    chng

---
+++ script.js
@@ -0,0 +1,36 @@
+var menuBtn = document.querySelector(".ri-menu-line");
+var navpart3 = document.querySelector(".nav-part3");
+var navpart2 = document.querySelector(".nav-part2");
+var cancelBtn = document.querySelector(".ri-close-fill");
+// var toggleNavStatus = false;
+console.log("1");
+
+menuBtn.addEventListener("click", toggle);
+cancelBtn.addEventListener("click", toggleOff);
+
+function toggle(){
+    if(menuBtn.style.display == "block") { // if is menuBox displayed, hide it
+        menuBtn.style.display = "none";
+        cancelBtn.style.display = "block";
+        navpart2.style.display = "none";
+        
+        navpart3.style.display = "flex";
+        navpart3.style.flexDirection = "column";
+    }
+    else { // if is menuBox hidden, display it
+        menuBtn.style.display = "block";
+    }
+}
+
+function toggleOff(){
+    if(cancelBtn.style.display == "block") { // if is menuBox displayed, hide it
+        cancelBtn.style.display = "none";
+        menuBtn.style.display = "block";
+        navpart3.style.display = "none";
+        navpart3.style.flexDirection = "column";
+    }
+    else { // if is menuBox hidden, display it
+        cancelBtn.style.display = "block";
+    }
+}
+
